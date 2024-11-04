5e3a23f560c34d71393331a840fd387b03a6b8a5
Davidsusairaj s <davidsusairaj.sebasthiyar@solverminds.com>
2024-11-04 17:19:23 +0530

    types added

---
+++ types/david-unique-id.d.ts
@@ -0,0 +1,9 @@
+declare module 'david-unique-id' {
+    /**
+     * Returns a unique identifier based on the current timestamp.
+     * @returns {number} - The current timestamp.
+     */
+    function davidUniqueId(): number;
+    export = davidUniqueId;
+  }
+  
